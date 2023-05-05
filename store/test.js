7761687709a4083c387ecdd83ac78996c0034ca7
LEales <102584475+LEales@users.noreply.github.com>
2023-05-05 10:32:43 +0200

    added random products to first page

--- store/test.js
+++ store/test.js
@@ -1,5 +1,7 @@
 var connection = new WebSocket('ws://127.0.0.1:1234')
 
+
+
 connection.onopen = function() {
     data = {}
     payload = {}
@@ -8,7 +10,7 @@ connection.onopen = function() {
     payload.price = 10
     payload.date = '1111'
     payload.colour = 'red'
-    payload.condition = 1
+    payload.condition = 'good'
     payload.productName = 'hej'
     data.payload = payload
     console.log('connected')
@@ -22,6 +24,22 @@ connection.onmessage = function(evt) {
         handleLogin(payload.success)
     } else if (data.type === 'signup') {
         handleSignup(payload)
+    } else if (data.type === 'randomProducts') {
+        productContainer = document.getElementById('cock')
+        payload.forEach(element => {
+            const productTemplate = `<div class="product">
+                <div class="product-name">${element.productName}</div>
+                <div class="prodzuct-category">${element.productType}</div>
+                <div class="product-price">${element.price}</div>
+                <div class="product-year">${element.yearOfProduction}</div>
+                <div class="product-color">${element.colour}</div>
+                <div class="product-condition">${element.condition}</div>
+                <div class="product-seller">${element.seller}</div>
+                <button class="add-to-cart">Add to Cart</button>
+            </div>`
+            productContainer.innerHTML += productTemplate
+            console.log('hello')
+        });
     }
 }
 
